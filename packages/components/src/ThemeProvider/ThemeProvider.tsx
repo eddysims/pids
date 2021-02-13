6ac5f990558ec1f2d02e2c561431a90883869580
eddysims <eddy.s@getjobber.com>
2021-02-12 21:59:09 -0700

    adds a ThemeProvider component

---
+++ packages/components/src/ThemeProvider/ThemeProvider.tsx
@@ -0,0 +1,10 @@
+import React, { PropsWithChildren } from 'react';
+import { ThemeProvider as ThemeUiThemeProvider } from 'theme-ui';
+
+interface ThemeProviderProps {
+  readonly theme: any;
+}
+export function ThemeProvider({theme, children}: PropsWithChildren<ThemeProviderProps>) {
+    return <ThemeUiThemeProvider theme={theme}>{children}</ThemeUiThemeProvider>
+}
+

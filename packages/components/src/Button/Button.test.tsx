38ea4bf9786df5764826d770a4084fa48737778c
eddysims <eddy.s@getjobber.com>
2021-02-13 00:53:45 -0700

    test Button

--- packages/components/src/Button/Button.test.tsx
+++ packages/components/src/Button/Button.test.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { render, cleanup } from "@testing-library/react";
+import { render, cleanup, fireEvent } from "@testing-library/react";
 
 import { Button } from ".";
 
@@ -8,4 +8,13 @@ afterEach(cleanup);
 it("renders a Button", () => {
   const { getByText } = render(<Button label="Foo" onClick={jest.fn()} />);
   expect(getByText("Foo")).toBeInstanceOf(HTMLButtonElement);
+});
+
+it("calls the onClick method", () => {
+  const clickHandler = jest.fn();
+  const { getByText } = render(<Button label="Foo" onClick={clickHandler} />);
+  const button = getByText("Foo");
+
+  fireEvent.click(button);
+  expect(clickHandler).toHaveBeenCalledTimes(1);
 });

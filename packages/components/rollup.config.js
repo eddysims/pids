dd9294225e425f5adfcf2916f08bce7d2fc31aa3
eddysims <eddy.s@getjobber.com>
2021-02-15 10:41:27 -0700

    adds rollup to build

---
+++ packages/components/rollup.config.js
@@ -0,0 +1,14 @@
+import multiInput from "rollup-plugin-multi-input";
+import typescript from 'rollup-plugin-typescript2'
+
+export default {
+  input: `src/*/index.ts`,
+  plugins: [multiInput(), typescript({declarationDir: "dist"})],
+  output: [
+    {
+      dir: "dist",
+      format: "cjs",
+    },
+  ],
+  external: ["react", "classnames"],
+};

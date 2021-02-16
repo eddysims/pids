2230315cd826482e3bbce5c2f85e63a5e3f75a52
eddysims <eddy.s@getjobber.com>
2021-02-15 20:48:52 -0700

    add rollup config

--- packages/components/rollup.config.js
+++ packages/components/rollup.config.js
@@ -1,9 +1,30 @@
 import multiInput from "rollup-plugin-multi-input";
-import typescript from 'rollup-plugin-typescript2'
+import typescript from "rollup-plugin-typescript2";
+import postcss from "rollup-plugin-postcss";
+import postCssPresetEnv from "postcss-preset-env";
 
 export default {
   input: `src/**/*/index.ts`,
-  plugins: [multiInput(), typescript({declarationDir: "dist"})],
+  plugins: [
+    multiInput(),
+    typescript({ declarationDir: "dist" }),
+    postcss({
+      modules: {
+        generateScopedName: "[hash:base64]",
+        globalModulePaths: [/node_modules/],
+      },
+      minimize: true,
+      plugins: [
+        postCssPresetEnv({
+          autoprefixer: true,
+          stage: 1,
+          browsers: "last 2 versions",
+          preserve: true,
+          importFrom: [],
+        }),
+      ],
+    }),
+  ],
   output: [
     {
       dir: "dist",

3c84314b44e5d9988c0237a3832cdfa45f179810
eddysims <eddy.s@getjobber.com>
2021-02-18 23:10:05 -0700

    fix linting

--- packages/components/src/Heading/Heading.tsx
+++ packages/components/src/Heading/Heading.tsx
@@ -3,6 +3,7 @@ import React, { PropsWithChildren } from "react";
 interface HeadingProps {
   readonly as?: "h1" | "h2" | "h3" | "h4" | "h5" | "h6";
 }
+
 export function Heading({
   as = "h2",
   children,

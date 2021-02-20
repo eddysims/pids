865ce81f2436eeea571d4423eb2efa003b6b47b5
eddysims <eddy.s@getjobber.com>
2021-02-19 17:13:49 -0700

    adds the navigation

--- src/gatsby-theme-docz/components/Layout/Layout.tsx
+++ src/gatsby-theme-docz/components/Layout/Layout.tsx
@@ -1,36 +1,20 @@
 import React, { PropsWithChildren } from "react";
-import { useMenus, Link } from "docz";
 import { Header } from "../Header";
+import { Sidebar } from "../Sidebar";
+import styles from "./Layout.css";
 
 export function Layout({ children }: PropsWithChildren<unknown>) {
-  const menu = useMenus() || [];
-
   return (
-    <>
+    <div className={styles.layout}>
       <Header />
-      <div className="main">
-        {menu.map((item) => {
-          if (!item.menu) {
-            return <Link to={item.route || ""}>{item.name}</Link>;
-          }
-
-          return (
-            <div>
-              <hr />
-              {item.name}
-              {item.menu.map((i) => {
-                return (
-                  <>
-                    <br />
-                    <Link to={i.route || ""}>{i.name}</Link>
-                  </>
-                );
-              })}
-            </div>
-          );
-        })}
+      <div className={styles.main}>
+        <div className={styles.sidebar}>
+          <Sidebar />
+        </div>
+        <main className={styles.content}>
+          <div className={styles.container}>{children}</div>
+        </main>
       </div>
-      <div>{children}</div>
-    </>
+    </div>
   );
 }

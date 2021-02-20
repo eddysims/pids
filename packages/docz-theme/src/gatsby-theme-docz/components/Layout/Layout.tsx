e1ab5118413ffa425bbbe8cb0b954fe5f59cbc14
eddysims <eddy.s@getjobber.com>
2021-02-20 16:33:44 -0700

    adds the grid to layout

--- packages/docz-theme/src/gatsby-theme-docz/components/Layout/Layout.tsx
+++ packages/docz-theme/src/gatsby-theme-docz/components/Layout/Layout.tsx
@@ -1,5 +1,15 @@
 import React, { PropsWithChildren } from "react";
 
+import styles from "./Layout.css";
+
 export function Layout({ children }: PropsWithChildren<unknown>) {
-  return <div>{children}</div>;
+  return (
+    <div className={styles.wrapper}>
+      <header className={styles.header}>Header</header>
+      <nav>Sidebar</nav>
+      <main>
+        <div className={styles.container}>{children}</div>
+      </main>
+    </div>
+  );
 }

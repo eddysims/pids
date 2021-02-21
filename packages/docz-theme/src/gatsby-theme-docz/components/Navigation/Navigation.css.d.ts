e1b5e87171649327dd25109cf88aeb42969e0827
eddysims <eddy.s@getjobber.com>
2021-02-20 18:07:10 -0700

    adds Navigation component

---
+++ packages/docz-theme/src/gatsby-theme-docz/components/Navigation/Navigation.css.d.ts
@@ -0,0 +1,9 @@
+declare const styles: {
+  readonly "navigation": string;
+  readonly "item": string;
+  readonly "topLevelLink": string;
+  readonly "linkGroup": string;
+  readonly "levelTwoLink": string;
+};
+export = styles;
+
---
+++ packages/docz-theme/src/gatsby-theme-docz/components/Navigation/Navigation.tsx
@@ -0,0 +1,89 @@
+import React from "react";
+import { XOR } from 'ts-xor';
+import { useMenus, Link, MenuItem } from "docz";
+
+import styles from "./Navigation.css";
+
+export function Navigation() {
+  const menus = useMenus();
+
+  if (!menus) {
+    return <></>;
+  }
+
+  return (
+    <div className={styles.navigation}>
+      {menus.map((menu) => {
+        if (!menu.menu) {
+          return (
+            <div className={styles.item} key={menu.id}>
+              <TopLevelLink label={menu.name} url={menu.route as string} />
+            </div>
+          );
+        }
+
+        return (
+          <div className={styles.item} key={menu.id}>
+            <LinkGroup menu={menu} />
+          </div>
+        );
+      })}
+    </div>
+  );
+}
+
+interface TopLevelLinkBaseProps {
+  readonly label: string;
+}
+
+interface TopLevelLinkLinkProps extends TopLevelLinkBaseProps {
+  readonly url: string;
+}
+
+interface TopLevelLinkButtonProps extends TopLevelLinkBaseProps {
+  onClick(): void;
+}
+
+type TopLevelLinkProps = XOR<TopLevelLinkLinkProps, TopLevelLinkButtonProps>
+
+
+function TopLevelLink({ label, url, onClick }: TopLevelLinkProps) {
+  if (url) {
+    return (
+      <Link to={url} className={styles.topLevelLink}>
+        {label}
+      </Link>
+    );
+  }
+
+  return <button type="button" onClick={onClick} className={styles.topLevelLink}>{label}</button>
+}
+
+interface LinkGroupProps {
+  readonly menu: MenuItem;
+}
+
+function LinkGroup({ menu }: LinkGroupProps) {
+
+  if (!menu.menu) {
+    return <></>
+  }
+
+  return (
+    <>
+      <TopLevelLink label={menu.name} onClick={handleClick} />
+      <div className={styles.linkGroup}>
+        {menu.menu.map(menu => {
+          return (
+            <Link to={menu.route as string} className={styles.levelTwoLink}>{menu.name}</Link>
+          )
+        })}
+      </div>
+
+    </>
+  );
+
+  function handleClick() {
+    alert('hi')
+  }
+}

750f41bb27d3e1e226d445d6eef2b65959474073
eddysims <eddy.s@getjobber.com>
2021-02-21 16:22:15 -0700

    adds Icon into the Button

--- packages/components/src/Button/Button.tsx
+++ packages/components/src/Button/Button.tsx
@@ -1,15 +1,38 @@
 import React from "react";
+import { XOR } from "ts-xor";
+import { Icon, IconType } from "../Icon";
 
 import styles from "./Button.css";
 
-interface ButtonProps {
-  readonly label: string;
+interface ButtonBaseProps {
   onClick(): void;
 }
 
-export function Button({ label = "Button", onClick }: ButtonProps) {
+interface ButtonIconProps extends ButtonBaseProps {
+  readonly icon: IconType;
+  /**
+   * The label that is to be read by screen readers.
+   */
+  readonly ariaLabel: string;
+  readonly label?: string;
+}
+
+interface ButtonNoIconProps extends ButtonBaseProps {
+  readonly label: string;
+  readonly icon?: IconType;
+}
+
+type ButtonProps = XOR<ButtonIconProps, ButtonNoIconProps>;
+
+export function Button({ label, icon, ariaLabel, onClick }: ButtonProps) {
   return (
-    <button type="button" onClick={handleClick} className={styles.button}>
+    <button
+      type="button"
+      onClick={handleClick}
+      className={styles.button}
+      aria-label={ariaLabel}
+    >
+      {icon && <Icon icon={icon} />}
       {label}
     </button>
   );

ec2d502b85a6894167a2f6d411bdc4734b2c77d8
eddysims <eddy.s@getjobber.com>
2021-02-24 15:07:09 -0700

    Update Heading documentation

--- packages/docz-theme/src/gatsby-theme-docz/components/Layout/Layout.tsx
+++ packages/docz-theme/src/gatsby-theme-docz/components/Layout/Layout.tsx
@@ -1,10 +1,14 @@
 import React, { PropsWithChildren } from "react";
+import { useCurrentDoc } from 'docz';
+import { Heading } from '@pids/components/Heading';
 import { Header } from "../Header";
 import { Sidebar } from "../Sidebar";
 
 import styles from "./Layout.css";
 
 export function Layout({ children }: PropsWithChildren<unknown>) {
+  const { name, description } = useCurrentDoc();
+
   return (
     <div className={styles.wrapper}>
       <header className={styles.header}>
@@ -13,7 +17,14 @@ export function Layout({ children }: PropsWithChildren<unknown>) {
       <nav>
         <Sidebar />
       </nav>
-      <main>
+      <main className={styles.main}>
+        <div className={styles.hero}>
+          <div className={styles.container}>
+            <Heading as="h1">{name}</Heading>
+            {description}
+          </div>
+        </div>
+
         <div className={styles.container}>{children}</div>
       </main>
     </div>

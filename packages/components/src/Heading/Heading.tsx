a69e0ff0cc41fed3c1d4a93e00222ea41c046e95
Eddy Sims <46460840+eddysims@users.noreply.github.com>
2021-02-24 15:16:01 -0700

    feat: add styling to Heading (#15)
    * adds some font-size variables
    
    * add some styles to the Heading component
    
    * Update Heading documentation
    
    * make linter happy

--- packages/components/src/Heading/Heading.tsx
+++ packages/components/src/Heading/Heading.tsx
@@ -1,13 +1,18 @@
 import React, { PropsWithChildren } from "react";
+import classnames from "classnames";
+
+import styles from "./Heading.css";
 
 interface HeadingProps {
-  readonly as?: "h1" | "h2" | "h3" | "h4" | "h5" | "h6";
+  readonly as?: "h1" | "h2";
 }
 
 export function Heading({
   as = "h2",
   children,
 }: PropsWithChildren<HeadingProps>) {
+  const headingClass = classnames(styles.heading, styles[as]);
   const Element = as;
-  return <Element>{children}</Element>;
+
+  return <Element className={headingClass}>{children}</Element>;
 }
--- packages/docz-theme/src/gatsby-theme-docz/components/Layout/Layout.css.d.ts
+++ packages/docz-theme/src/gatsby-theme-docz/components/Layout/Layout.css.d.ts
@@ -2,6 +2,8 @@ declare const styles: {
   readonly "wrapper": string;
   readonly "header": string;
   readonly "container": string;
+  readonly "main": string;
+  readonly "hero": string;
 };
 export = styles;
 

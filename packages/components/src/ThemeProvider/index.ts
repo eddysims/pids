71c4040c602bd33eef507e47d2fb8dffdfec70af
eddysims <eddy.s@getjobber.com>
2021-03-03 21:06:17 -0700

    add the themeprovider component

---
+++ packages/components/src/ThemeProvider/index.ts
@@ -0,0 +1,1 @@
+export { ThemeProvider, useColorMode } from "./ThemeProvider";
--- packages/docz-theme/src/gatsby-theme-docz/wrapper.tsx
+++ packages/docz-theme/src/gatsby-theme-docz/wrapper.tsx
@@ -1,10 +1,11 @@
 import React, { PropsWithChildren } from "react";
+import { ThemeProvider } from "@pids/components/ThemeProvider";
 
 // eslint-disable-next-line import/no-unresolved
 import "@pids/styles/styles.css";
 
 function Wrapper({ children }: PropsWithChildren<unknown>) {
-  return <>{children}</>;
+  return <ThemeProvider>{children}</ThemeProvider>;
 }
 
 export default Wrapper;
